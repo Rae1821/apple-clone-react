32199c8b8861becbd6cb7b5c132f170ee2688d17
Rachel Dooley <102691550+Rae1821@users.noreply.github.com>
2024-03-25 21:44:58 -0400

    Added main components

---
+++ src/components/VideoCarousel.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { hightlightsSlides } from "../constants";
+
+const VideoCarousel = () => {
+  return (
+    <>
+      <div className="flex items-center">
+        {hightlightsSlides.map((list, index) => (
+          <div key={list.id} id="slider" className="sm:pr-20 pr-10">
+            <div className="video-carousel_container">
+              <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
+                <video>
+                  <source src={list.video} />
+                </video>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default VideoCarousel;
